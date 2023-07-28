ad116ee3248469493b7e4d5108055477850a6546
Soufiane Joumal <joumalsoufian17@gmail.com>
2023-07-28 20:22:52 +0100

    changes

--- src/components/work/WorkItems.jsx
+++ src/components/work/WorkItems.jsx
@@ -5,9 +5,6 @@ const WorkItems = ({item}) => {
     <div className="work__card" key={item.id}>
       <img src={item.image} alt="" className='work__img'/>
       <h3 className="work__title">{item.title}</h3>
-      <a href="https://github.com/SoufianeJm" className="work__button">
-        Demo <i className="bx bx-right-arrow-alt work__button-icon"></i>
-      </a>  
       </div>
   );
 }
